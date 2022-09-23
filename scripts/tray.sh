16448f0deeb13cdd3f76000ef796f61daa39a02d
Yi Fong Au <yifongau@gmail.com>
2022-09-24 00:45:51 +0200

    lock screen + refactoring

--- scripts/tray.sh
+++ scripts/tray.sh
@@ -3,7 +3,9 @@
 pkill -f pasystray
 pkill -f blueman-appplet
 pkill -f nm-applet
+pkill -f xfce4-power-manager
 
 pasystray --notify=all &
 blueman-applet &
 nm-applet --indicator &
+xfce4-power-manager 
